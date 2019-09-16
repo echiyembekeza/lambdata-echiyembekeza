607fcb4fec1198479ea09aaec14b9d9268b63852
echiyembekeza <username@users.noreply.github.com>
2019-09-15 21:15:09 -0400

    Added choose stock class

---
+++ lambdata_echiyembekeza/choose_stock.py
@@ -0,0 +1,17 @@
+import quandl
+import pandas as pd
+
+# Choose the stock
+# symbol = 'EOD/AAPL'
+# start = '1988-01-01'
+# end = '2015-12-31'
+# data_raw = get_data_quandl(symbol, start, end)
+# data = generate_features(data_raw)
+
+
+class Choose_Stock:
+    """Select the stock of choice"""
+    def __init__(self, symbol='EOD/AAPL', start='1988-01-01', end='2015-12-31'):
+        self.symbol = name
+        self.start = price
+        self.end = weight
